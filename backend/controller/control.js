1d1fc1261fbf11685feb3b9ca4a73c6056517c7f
Namit Yadav <namityadav5255@gmail.com>
2025-07-07 22:20:15 +0530

    new controller added for getting id on screen

--- backend/controller/control.js
+++ backend/controller/control.js
@@ -4,15 +4,35 @@ const model = require("../model/schema");
 async function handlerurlpost(req,res){
     const body=req.body
     const generate=shortid.generate()
-await model.create({
+    if ((!body.url.startsWith("http://"))&&(!body.url.startsWith("https://"))){
+      httpurl="http://"+body.url
+      await model.create({
     shortId:generate,
-    redirecturl:body.url
+    redirecturl:httpurl
 })
+    }
+
 res.status(201).json({
       message: "Short URL created",
       shortid: generate,
-      redirecturl:body.url
+      redirecturl:httpurl
     });
   } 
+  
 
-module.exports={handlerurlpost}
+async function handlerdataprint(req,res) {
+  const id=req.params.id
+  const findid= await model.findOne({shortId:id})
+  if(!findid){
+    res.status(500).json({
+      message:"not found"
+    })
+  }
+  else{
+    res.redirect(findid.redirecturl)
+  }
+
+ 
+}
+
+module.exports={handlerurlpost,handlerdataprint}
